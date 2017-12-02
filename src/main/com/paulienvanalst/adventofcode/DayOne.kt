890bed18df2b100f3762a49b5c86a670d4c29c30
Paulien van Alst <you@example.com>
2017-12-02 22:17:32 +0100

    DayOne refactored

--- src/main/com/paulienvanalst/adventofcode/DayOne.kt
+++ src/main/com/paulienvanalst/adventofcode/DayOne.kt
@@ -1,53 +1,25 @@
 package com.paulienvanalst.adventofcode
 
 
-object InverseCaptcha {
+object Captcha {
 
-    fun calculate(input: String): Int {
+    fun calculateInverse(input: String): Int {
         val values = input.toCharArray().map { Integer.valueOf(it.toString()) }
-        return determineSum(values.iterator(), IntermediateHash(0, initialAccumulator(values)))
-    }
-
-    private fun initialAccumulator(values: List<Int>): Int {
-        if (values.first() == values.last()) {
-            return values.first()
-        }
-        return 0
-    }
-
-    private fun determineSum(values: Iterator<Int>, previousHash: IntermediateHash): Int {
-        if (values.hasNext()) {
-            val nextValue = values.next()
-            if (nextValue == previousHash.previousValue) {
-                return determineSum(values, IntermediateHash(nextValue, previousHash.acc + nextValue))
-            }
-            return determineSum(values, IntermediateHash(nextValue, previousHash.acc))
-        }
-        return previousHash.acc
+        return captcha(values, 1)
 
     }
 
-    data class IntermediateHash(val previousValue: Int, val acc: Int)
+    fun calculateCircular(input: String): Int {
+        val values = input.toCharArray().map { Integer.valueOf(it.toString()) }
+        return captcha(values, input.length / 2)
+    }
+
+
+    private fun captcha(values: List<Int>, stepSize: Int): Int {
+        return values.mapIndexed { index, number ->
+            if (values[(index + stepSize) % values.size] == number) number else 0
+        }.sum()
+    }
 }
 
-object CircularInverseCaptcha {
-    fun calculate(input: String): Int {
-        val stepSize = input.length / 2
-        val values = input.toCharArray().map { Integer.valueOf(it.toString()) }
-        return determineSum(values, 0, stepSize)
-    }
 
-    private fun determineSum(values: List<Int>, sum: Int, stepSize: Int): Int {
-        if (values.isEmpty() || values.size <= stepSize) {
-            return sum
-        }
-
-        if (values.first() == values[stepSize]){
-            return determineSum(values.tail(), sum + values.first() + values[stepSize], stepSize)
-        }
-        return determineSum(values.tail(), sum, stepSize)
-    }
-
-    private fun List<Int>.tail() = this.subList(1, this.size)
-
-}
