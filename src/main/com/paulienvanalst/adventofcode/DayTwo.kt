60918ab09b4daac688e769a02d32612c3cd8fa3c
Paulien van Alst <you@example.com>
2017-12-02 22:34:31 +0100

    Day Two - refactor all the things

--- src/main/com/paulienvanalst/adventofcode/DayTwo.kt
+++ src/main/com/paulienvanalst/adventofcode/DayTwo.kt
@@ -1,26 +1,31 @@
 package com.paulienvanalst.adventofcode
 
-object MaxMinCheckSum {
-    fun calculate(linesTable: List<String>): Int = linesTable
-        .map {
-            val numbers = it.mapToIntList()
-            numbers.max()!! - numbers.min()!!
-        }
-        .sum()
-}
+object CheckSum {
+    fun minMax (linesTable: List<String>): Int = linesTable.checksum(minMax)
 
-object EvenlyDividableChecksum {
-    fun calculate(linesTable: List<String>) : Int = linesTable
-        .map {
-            quotientOfEvenlyDividable(it.mapToIntList().sortedDescending())
-        }.sum()
+    fun evenDividable (linesTable: List<String>) : Int = linesTable.checksum(quotient)
 
-    private fun quotientOfEvenlyDividable(line: List<Int>): Int {
+    private fun List<String>.checksum(calculationMethod: (String) -> Int): Int {
+        return this
+                .map {calculationMethod (it)}
+                .sum()
+    }
+
+    private val minMax: (it: String) -> Int = {
+        val numbers = it.mapToIntList()
+        numbers.max()!! - numbers.min()!!
+    }
+
+    private val quotient: (it: String) -> Int = {
+        val line = it.mapToIntList().sortedDescending()
         val evenDivision = line.flatMap {
             (line - it).filter { rest -> it % rest == 0  || rest % it == 0}
         }
-        return evenDivision.max()!! / evenDivision.min()!!
+        evenDivision.max()!! / evenDivision.min()!!
     }
+
+    private fun String.mapToIntList() : List<Int> = this.split(" ").map { Integer.valueOf(it) }
 }
 
-fun String.mapToIntList() : List<Int> = this.split(" ").map { Integer.valueOf(it) }
+
+
--- src/test/com/paulienvanalst/adventofcode/DayTwoTest.kt
+++ src/test/com/paulienvanalst/adventofcode/DayTwoTest.kt
@@ -8,13 +8,13 @@ class D2P1Test {
     @Test
     fun `table should result in checksum of 18` () {
         val linesTable = File(ClassLoader.getSystemResource("examples/DayTwo_PartOne.txt").file).readLines()
-        assertEquals(18, MaxMinCheckSum.calculate(linesTable))
+        assertEquals(18, CheckSum.minMax(linesTable))
     }
 
     @Test
     fun `my own puzzle input`() {
         val linesTable = File(ClassLoader.getSystemResource("input/DayTwo_PartOne.txt").file).readLines()
-        print( MaxMinCheckSum.calculate(linesTable) )
+        assertEquals(34581, CheckSum.minMax(linesTable))
     }
 }
 
@@ -22,14 +22,14 @@ class D2P2Test {
     @Test
     fun `table should rest in checksum of 9`() {
         val linesTable = File(ClassLoader.getSystemResource("examples/DayTwo_PartTwo.txt").file).readLines()
-        assertEquals(9, EvenlyDividableChecksum.calculate(linesTable))
+        assertEquals(9, CheckSum.evenDividable(linesTable))
     }
 
 
     @Test
     fun `my own puzzle input`() {
         val linesTable = File(ClassLoader.getSystemResource("input/DayTwo_PartTwo.txt").file).readLines()
-        print(EvenlyDividableChecksum.calculate(linesTable))
+        assertEquals(214, CheckSum.evenDividable(linesTable))
     }
 
 }
