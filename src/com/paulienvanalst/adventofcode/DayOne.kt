01662fac4f8149fb3ef8eb566fb22fe025154730
Paulien van Alst <paulienvalst@gmail.com>
2017-12-02 09:33:04 +0100

    First attempt day 1

--- src/com/paulienvanalst/adventofcode/DayOne.kt
+++ src/com/paulienvanalst/adventofcode/DayOne.kt
@@ -1,6 +1,24 @@
 package com.paulienvanalst.adventofcode
 
 
-object DayOne {
+object InverseCaptcha {
 
+    fun calculate(input: String): Int {
+        return sumTheNeighbours(input.iterator(), determineStartSum(input), "")
+    }
+
+    private fun sumTheNeighbours(input: CharIterator, sum: Int, previousChar: String): Int {
+        if (input.hasNext()){
+            val nextChar = input.nextChar().toString()
+            compareTwoCharAndDetermineSum(previousChar, nextChar, sum)
+            return sumTheNeighbours(input, sum, nextChar)
+        }
+        return sum
+    }
+
+    private fun compareTwoCharAndDetermineSum(previousChar: String, nextChar: String, sum: Int): Integer {
+        if (previousChar == nextChar) {
+            return Integer.valueOf(previousChar) + Integer.valueOf(nextChar)
+        }
+    }
 }
