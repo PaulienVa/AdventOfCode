1bc418def89ae309119c34138afa2866c359dd28
Paulien van Alst <you@example.com>
2017-12-02 13:51:13 +0100

    Day one Part Two

--- src/com/paulienvanalst/adventofcode/DayOne.kt
+++ src/com/paulienvanalst/adventofcode/DayOne.kt
@@ -22,15 +22,32 @@ object InverseCaptcha {
                 return determineSum(values, IntermediateHash(nextValue, previousHash.acc + nextValue))
             }
             return determineSum(values, IntermediateHash(nextValue, previousHash.acc))
-
         }
-
         return previousHash.acc
 
     }
 
     data class IntermediateHash(val previousValue: Int, val acc: Int)
+}
 
+object CircularInverseCaptcha {
+    fun calculate(input: String): Int {
+        val stepSize = input.length / 2
+        val values = input.toCharArray().map { Integer.valueOf(it.toString()) }
+        return determineSum(values, 0, stepSize)
+    }
 
+    private fun determineSum(values: List<Int>, sum: Int, stepSize: Int): Int {
+        if (values.isEmpty() || values.size <= stepSize) {
+            return sum
+        }
+
+        if (values.first() == values[stepSize]){
+            return determineSum(values.tail(), sum + values.first() + values[stepSize], stepSize)
+        }
+        return determineSum(values.tail(), sum, stepSize)
+    }
+
+    private fun List<Int>.tail() = this.subList(1, this.size)
 
 }
